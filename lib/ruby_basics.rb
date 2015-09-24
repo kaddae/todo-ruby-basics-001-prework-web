00ca88a32ac53f353aa5ac184ba6390c053b074c
kaddae <kai@tilde.io>
2015-09-24 15:23:11 -0700

    basic

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  name= value.to_s
 end
 
-def argue
-  
+def argue(method)
+  method
 end
 
-def greeting
+def greeting(word, word2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping= "cheese")
+  topping
 end
